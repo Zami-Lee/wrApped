ec13f2991be90e4cd14538e3c713e765fb3cfed7
Zami-Lee <zlee5358@gmail.com>
2023-07-18 03:04:34 +1000

    added backend and signup working with chrome

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
 
+import 'auth_page.dart';
 import 'explore_page/explore_page.dart';
 import 'home_page/home_page.dart';
 import 'photo_page/photo_page.dart';
 
-void main() => runApp(FoodSocialMediaApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(FoodSocialMediaApp());
+}
 
 Color appBarColour = const Color(0xFFF88DAD);
 Color appBarTextColour = const Color(0xFF6F5E76);
@@ -17,51 +27,64 @@ class FoodSocialMediaApp extends StatefulWidget {
 class _FoodSocialMediaAppState extends State<FoodSocialMediaApp> {
   int currentIndex = 0;
   final List<Widget> screens = [
-    const HomePage(),
+    const AuthPage(),
+    const HomePage(), // Initially set to AuthPage
     ExplorePage(),
     PhotoSharingPage(),
   ];
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       title: 'wrApped',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'wrApped',
-            style: TextStyle(
-              color: appBarTextColour,
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
+      home: StreamBuilder<User>(
+        stream: FirebaseAuth.instance.authStateChanges().cast<User>(), // Cast the stream to User
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          } else if (snapshot.hasData) {
+            // User is authenticated, show the home page
+            return Scaffold(
+              appBar: AppBar(
+                title: Text(
+                  'wrApped',
+                  style: TextStyle(
+                    color: appBarTextColour,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                backgroundColor: appBarColour,
               ),
-            ),
-          backgroundColor: appBarColour,
-        ),
-        body: screens[currentIndex],
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: currentIndex,
-          onTap: (int index) {
-            setState(() {
-              currentIndex = index;
-            });
-          },
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.explore_off_rounded),
-              label: 'Explore',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.camera_alt),
-              label: 'Share',
-            ),
-          ],
-        ),
+              body: screens[currentIndex],
+              bottomNavigationBar: BottomNavigationBar(
+                currentIndex: currentIndex,
+                onTap: (int index) {
+                  setState(() {
+                    currentIndex = index;
+                  });
+                },
+                items: const [
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.home),
+                    label: 'Home',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.explore_off_rounded),
+                    label: 'Explore',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.camera_alt),
+                    label: 'Share',
+                  ),
+                ],
+              ),
+            );
+          } else {
+            // User is not authenticated, show the authentication page
+            return const AuthPage();
+          }
+        },
       ),
     );
   }
